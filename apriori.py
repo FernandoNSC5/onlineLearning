b0a993c83d9d72e0d43c3bf62a02a8d48dc98f7f
Dautomne_ <fernandonsc5@gmail.com>
2020-01-24 11:05:11 -0300

    Working version, rules fix'

--- apriori.py
+++ apriori.py
@@ -1,13 +1,19 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
+import sys
 import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
+
+print('Starting')
 
 ####################################################
 ##
 ##	Variables
-data = pd.read_excel('Online_retail.xlsx')
-data.head()
-print('Data ready')
+data = pd.read_excel('Online_retail.xlsx') 
+data.head() 
+data.columns 
+data.Country.unique() 
 
 #####################################################
 ##
@@ -25,16 +31,11 @@ def encode(x):
 #####################################################
 ##
 ##	Limpando dados
-print('Cleaning data.', end='')
+print('Cleaning data.')
 data['Description'] = data['Description'].str.strip()
-
-#Descartando linhas sem número de fatura
-data.dropna(axis=0, subset=['InvoiceNo'], inplace = True)
+data.dropna(axis = 0, subset =['InvoiceNo'], inplace = True) 
 data['InvoiceNo'] = data['InvoiceNo'].astype('str')
-print('.', end='')
-#Descartando todas as transações pagas no crédito
-data = data[~data['InvoiceNo'].str.contains('C')]
-print(' finished')
+data = data[~data['InvoiceNo'].str.contains('C')] 
 ##
 ##	Fim da limpeza
 #####################################################
@@ -43,28 +44,25 @@ print(' finished')
 ##
 ##	Segregando por região
 print('unpatching.', end='')
-basket_France = (data[data['Country'] == "France"]
-	.groupby(['InvoiceNo', 'Description'])['Quantity']
-	.sum().unstack().reset_index().fillna(0)
-	.set_index("InvoiceNo"))
-
-print('.', end='')
-basket_UK = (data[data['Country'] == "United Kingdom"]
-	.groupby(['InvoiceNo', 'Description'])['Quantity']
-	.sum().unstack().reset_index().fillna(0)
-	.set_index("InvoiceNo"))
-
-print('.', end='')
-basket_Portugal = (data[data['Country'] == "Portugal"]
-	.groupby(['InvoiceNo', 'Description'])['Quantity']
-	.sum().unstack().reset_index().fillna(0)
-	.set_index("InvoiceNo"))
-
-print('.', end='')
-basket_Sweden = (data[data['Country'] == "Sweden"]
-	.groupby(['InvoiceNo', 'Description'])['Quantity']
-	.sum().unstack().reset_index().fillna(0)
-	.set_index("InvoiceNo"))
+basket_France = (data[data['Country'] =="France"] 
+          .groupby(['InvoiceNo', 'Description'])['Quantity'] 
+          .sum().unstack().reset_index().fillna(0) 
+          .set_index('InvoiceNo')) 
+  
+basket_UK = (data[data['Country'] =="United Kingdom"] 
+          .groupby(['InvoiceNo', 'Description'])['Quantity'] 
+          .sum().unstack().reset_index().fillna(0) 
+          .set_index('InvoiceNo')) 
+  
+basket_Por = (data[data['Country'] =="Portugal"] 
+          .groupby(['InvoiceNo', 'Description'])['Quantity'] 
+          .sum().unstack().reset_index().fillna(0) 
+          .set_index('InvoiceNo')) 
+  
+basket_Sweden = (data[data['Country'] =="Sweden"] 
+          .groupby(['InvoiceNo', 'Description'])['Quantity'] 
+          .sum().unstack().reset_index().fillna(0) 
+          .set_index('InvoiceNo')) 
 print(' finished')
 ##
 ##	Fim da segregação por região
@@ -74,17 +72,17 @@ print(' finished')
 ##
 ##	Conversão de dados
 print('Encoding.', end='')
-basket_encoded = basket_France.applymap(encode)
-basketFrance = basket_encoded
-print('.', end='')
-basket_encoded = basket_UK.applymap(encode)
-basket_UK = basket_encoded
-print('.', end='')
-basket_encoded = basket_Portugal.applymap(encode)
-basket_Portugal = basket_encoded
-print('.', end='')
-basket_encoded = basket_Sweden.applymap(encode)
-basket_Sweden = basket_encoded
+basket_encoded = basket_France.applymap(encode) 
+basket_France = basket_encoded 
+  
+basket_encoded = basket_UK.applymap(encode) 
+basket_UK = basket_encoded 
+  
+basket_encoded = basket_Por.applymap(encode) 
+basket_Por = basket_encoded 
+  
+basket_encoded = basket_Sweden.applymap(encode) 
+basket_Sweden = basket_encoded 
 print(' finished')
 ##
 ##	Fim de conversão
@@ -95,7 +93,8 @@ print(' finished')
 ##	Criando modelos
 
 ###########	FRANCE ###############
-freq_items = apriori(basket_France, min_support=0.05, use_culnames = True)
-rules = association_rules(freq_items, metric = "lift", min_threshold = 1)
-rules = rules.sort_values(['confidence', 'lift'], ascending = [False, False])
+frq_items = apriori(basket_France, min_support = 0.05, use_colnames = True) 
+  
+rules = association_rules(frq_items, metric ="lift", min_threshold = 1) 
+rules = rules.sort_values(['confidence', 'lift'], ascending =[False, False]) 
 print(rules.head())
