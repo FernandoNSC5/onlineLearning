b621f3d33ff1e978ef735c6e0f28098a4c92c097
Fernando Nogueira da Silva Costa <fernandonsc5@gmail.com>
2020-01-30 20:04:21 -0300

    Updater is working

--- server/data_process.py
+++ server/data_process.py
@@ -7,56 +7,94 @@ from mlxtend.frequent_patterns import apriori, association_rules
 class Data():
 
 	def __init__(self):
-		print('Starting... ', end='')
-		self.data = pd.read_excel('Online_retail.xlsx')
-		self.data.head()
-		self.data.columns
+		print('[STARTING]\tLoading data')
+		self.data = pd.read_excel('Online_retail.xlsx') 
+		self.data.head() 
+		self.data.columns 
 		self.data.Country.unique()
-		print('Working')
-
+		print('[LOADED]\tCleaning data') 
 		self.clean_data()
-		self.unpatching_region()
+		print('[DATA CLEANED]\tRegion segmentation')
+		self.region_seg()
+		print('[SEGMENTED]\tEncoding')
+		self.encoding()
+		print('[ENCODED]\tCreating models')
+		self.models = self.create_models()
+		print('[MODELS READY]\n')
+		print(self.models)
 
-	def encode(x):
-		if x <= 0:
-			return 0
-		if x >= 1:
-			return 1
+	def update_data(self):
+		print('[UPDATING DATA]\t Reloading data')
+		self.data = pd.read_excel('Online_retail.xlsx') 
+		self.data.head() 
+		self.data.columns 
+		self.data.Country.unique()
+		print('[RELOADED]\tCleaning data') 
+		self.clean_data()
+		print('[DATA CLEANED]\tRegion segmentation')
+		self.region_seg()
+		print('[RESEGMENTED]\tEncoding')
+		self.encoding()
+		print('[REENCODED]\tCreating models')
+		self.models = self.create_models()
+		print('[NEW MODELS READY]\n')
+		print(self.models)
 
 	def clean_data(self):
-		print('Cleaning data', end='.')
-		#strip -> removes \n, \t, etc from strings
-		self.data['Description'] = self.data['Description'].str.strip()
-		print('.', end='')
-		#Drops missing values (nulls, numpy nan)
-		self.data.dropna(axis = 0 , subset = ['InvoiceNo'], inplace = True)
-		print('.', end='')
+		self.data['Description'] = self.data['Description'].str.strip() #strip -> removes \n, \t, etc from strings
+		self.data.dropna(axis = 0, subset =['InvoiceNo'], inplace = True) #Drops missing values (nulls, numpu nan) 
 		self.data['InvoiceNo'] = self.data['InvoiceNo'].astype('str')
-		self.data = self.data[~self.data['InvoiceNo'].str.contains('C')]
-		print('Done')
+		self.data = self.data[~self.data['InvoiceNo'].str.contains('C')] 
 
-	def unpatching_region(self):
-		print('unpatching.', end='')
-		basket_France = (self.data[self.data['Country'] =="France"] 
+	def region_seg(self):
+		self.france_balance = (self.data[self.data['Country'] =="France"] 
 		          .groupby(['InvoiceNo', 'Description'])['Quantity'] 
 		          .sum().unstack().reset_index().fillna(0) 
 		          .set_index('InvoiceNo')) 
 		  
-		basket_UK = (self.data[self.data['Country'] =="United Kingdom"] 
+		self.uk_balance = (self.data[self.data['Country'] =="United Kingdom"] 
 		          .groupby(['InvoiceNo', 'Description'])['Quantity'] 
 		          .sum().unstack().reset_index().fillna(0) 
 		          .set_index('InvoiceNo')) 
 		  
-		basket_Por = (self.data[self.data['Country'] =="Portugal"] 
+		self.portugal_balance = (self.data[self.data['Country'] =="Portugal"] 
 		          .groupby(['InvoiceNo', 'Description'])['Quantity'] 
 		          .sum().unstack().reset_index().fillna(0) 
 		          .set_index('InvoiceNo')) 
 		  
-		basket_Sweden = (self.data[self.data['Country'] =="Sweden"] 
+		self.sweden_balance = (self.data[self.data['Country'] =="Sweden"] 
 		          .groupby(['InvoiceNo', 'Description'])['Quantity'] 
 		          .sum().unstack().reset_index().fillna(0) 
 		          .set_index('InvoiceNo')) 
-		print(' finished')
-	
+
+	def encoding(self):
+		self.encoded_balance = self.france_balance.applymap(self.encode) 
+		self.france_balance = self.encoded_balance 
+		  
+		self.encoded_balance = self.uk_balance.applymap(self.encode) 
+		self.uk_balance = self.encoded_balance 
+		  
+		self.encoded_balance = self.portugal_balance.applymap(self.encode) 
+		self.portugal_balance = self.encoded_balance 
+		  
+		self.encoded_balance = self.sweden_balance.applymap(self.encode) 
+		self.sweden_balance = self.encoded_balance 
+
+	def create_models(self):
+
+		frq_items = apriori(self.france_balance, min_support = 0.05, use_colnames = True)   
+		rules = association_rules(frq_items, metric ="lift", min_threshold = 1) 
+		rules = rules.sort_values(['confidence', 'lift'], ascending =[False, False]) 
+		_antecedents_ = rules.head()['antecedents']
+		_consequents_ = rules.head()['consequents']
+		_confidence_ = rules.head()['confidence']
+
+		return rules.head()
+
+	def encode(self, x):
+		if x<=0:
+			return 0
+		if x>=1:
+			return 1
 
 d = Data()
