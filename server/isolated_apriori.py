45430559614bde9bdfd3f20aa042ac47041a98ff
Fernando Nogueira da Silva Costa <fernandonsc5@gmail.com>
2020-02-09 17:32:43 -0300

    Added logger info

--- server/isolated_apriori.py
+++ server/isolated_apriori.py
@@ -6,19 +6,11 @@ import data_process
 d = data_process.Data()
 
 #Lista de antecedentes para o apriori rodar
-antecedents = ['SET/20 RED RETROSPOT PAPER NAPKINS', 'SET/6 RED SPOTTY PAPER CUPS']
+antecedents = ['SET/6 RED SPOTTY PAPER PLATES', 'SET/20 RED RETROSPOT PAPER NAPKINS']
 
 french = d.get_french_model()
 french_antecedents = french['antecedents']
 french_consequents = french['consequents']
-
-#Printing model base
-'''print("Model: ")
-index = 0
-print("Antecedent\t\tConsequent")
-for i in french_antecedents:
-	print(str(french_antecedents[index] + "\t" + str(french_consequents[index])))
-	index += 1'''
 
 n_c = list()
 n_a = list()
@@ -29,15 +21,24 @@ for i in french_antecedents:
 for i in french_consequents:
 	n_c.append(list(i))
 
+#Printing model base
+print("Model: ")
+index = 0
+print("Antecedent\t\tConsequent")
+for i in n_a:
+	print(str(n_a[index]) + "\t" + str(n_c[index]))
+	index += 1
+
 #Searching for results
 index = 0
-consequent = list()
+consequent = set()
 for i in n_a:
 	if not len(set(i).intersection(antecedents)):
-		consequent.append(n_c[index]) 
+		consequent.add(str(n_c[index])) 
 	index += 1
 
 #printing
+print('\n\nResponse:')
 for i in consequent:
 	print(i)
 		
